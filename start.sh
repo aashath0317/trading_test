a77e6312bcde94c8103c81df4b15163e807e5892
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-01-20 21:49:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -- !catch.txt
 wget https://codeload.github.com/aashath0317/trading_test/zip/refs/heads/main
 unzip main
-mv -y trading_test-main/* /home/akeelaashath2/
+mv trading_test-main/* /home/akeelaashath2/
 python3 main.py
