e7795159783d24d6e9fee336fbd9a1bdc9d40f17
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-01-21 09:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -357,8 +357,9 @@ def my_handler(client, message):
                         text = order+","+pair+","+str(tp)+","+str(sl)+" "+"  Triggering...\nFrom M15"
                         bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.HTML)      
             else:
-                        text = str(pair)
-                        bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.HTML)
+                        pair = "GBPJPY"
+                        text = order+","+pair+","+str(tp)+","+str(sl)+" "+"  Triggering...\nFrom M15"
+                        bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.HTML)    
         elif pair == [' \n', '\n', 'EUR / AUD\n', '\x0c']:
             pair = "EURAUD"
             text = order+","+pair+","+str(tp)+","+str(sl)+" "+"  Triggering...\nFrom M15"
