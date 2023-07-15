464dd3c14f976fdee02afa62a3c31314d12a291c
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2023-07-15 19:01:34 +0530

    Update run.py

--- run.py
+++ run.py
@@ -37,7 +37,6 @@ client.connect()
 channel_id = -1001963686318
 group_id = "@C2P_forex_bot"   #5007713837
 igroup = -990951103
-client.send_message(group_id, s_message)
 
 @client.on(events.NewMessage(chats=channel_id))
 async def my_event_handler(event):
