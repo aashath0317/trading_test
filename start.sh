b48a87f3c5030dcd1bec57d0aa47b1bc76c2d612
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2023-07-15 13:39:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-rm -r *
-wget https://codeload.github.com/aashath0317/trading_test/zip/refs/heads/main
-unzip main
-mv trading_test-main/* /usr/src/app/
-python3 main.py
+gunicorn app:app && python3 
